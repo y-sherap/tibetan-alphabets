311329dc797c43c8f0a61cddd1b673b0a90e9622
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-01-13 15:25:45 -0500

    testing media queries for info component

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -3,7 +3,7 @@ import './info.css'
 
 function Info() {
   return (
-    <div className='components-container'>
+    <div id='info-container'>
         <div className='infoTextDiv'>
           Lorem Ipsum arcu, dapibus eu luctus et, efficitur eleifend nisi. Maecenas vel nibh ornare, fermentum turpis nec, porta diam. Curabitur laoreet urna vitae posuere pretium. Nam sit amet rhoncus massa, at varius lectus. Vestibulum elementum elit sit amet magna aliquam, vitae tincidunt mauris placerat. Proin vel consequat quam, quis dapibus ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
         </div>
