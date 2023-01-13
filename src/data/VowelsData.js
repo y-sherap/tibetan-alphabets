71204b69ae9788323f5a9d88d1b404e80a8f1fa1
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-01-12 21:32:32 -0500

    created vowel folder - bp + a vowelsdata file w/ fake data

---
+++ src/data/VowelsData.js
@@ -0,0 +1,35 @@
+const VowelsData =
+    [
+        {
+            "id":1,
+            "name":"ka",
+            "image": './images/ka-1.png',
+            "mp3": "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+        },
+        {
+            "id":2,
+            "name":"kha",
+            "image":"./images/kha-2.png",
+            "mp3":"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+        },
+        {
+            "id":3,
+            "name":"ga",
+            "image": './images/ga-3.png',
+            "mp3":"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+        },
+        {
+            "id":4,
+            "name":"nga",
+            "image":"./images/nga-4.png",
+            "mp3":"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+        },
+        {
+            "id":5,
+            "name":"ca",
+            "image": './images/ca-5.png',
+            "mp3":"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+        }
+    ]
+
+export default VowelsData
